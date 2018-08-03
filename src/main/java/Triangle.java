cead36d655240f2497b9e97e35849ae52bface6e
1anchen <svgyc@hotmail.co.uk>
2018-08-03 01:57:27 +0100

    all test pass

--- src/main/java/Triangle.java
+++ src/main/java/Triangle.java
@@ -5,6 +5,7 @@ public class Triangle {
     public String determineShape(int sideA, int sideB, int sideC){
 
         if(sideA+sideB>sideC && sideB+sideC>sideA && sideC+sideA>sideB){
+            
             if(sideA == sideB && sideA == sideC){
                 return "Equilateral";
             }
