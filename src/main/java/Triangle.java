1b129180147740196d4543e281cadf745c918e51
1anchen <svgyc@hotmail.co.uk>
2018-08-03 01:53:26 +0100

    All Test Passed

--- src/main/java/Triangle.java
+++ src/main/java/Triangle.java
@@ -1,26 +1,25 @@
 public class Triangle {
 
-    int sideA;
-    int sideB;
-    int sideC;
 
-    public Triangle(int numberOne, int numberTwo, int numberThree){
 
-        this.sideA=numberOne;
-        this.sideB=numberTwo;
-        this.sideC=numberThree;
+    public String determineShape(int sideA, int sideB, int sideC){
+
+        if(sideA+sideB>sideC && sideB+sideC>sideA && sideC+sideA>sideB){
+            if(sideA == sideB && sideA == sideC){
+                return "Equilateral";
+            }
+            if(sideA == sideB || sideA == sideC || sideB ==sideC){
+                return "Isosceles";
+            }else {
+
+                return "Irregular";
+            }
+
+        }else{
+
+            return "Impossible";
+        }
 
     }
 
-    public int getSideA() {
-        return this.sideA;
-    }
-
-    public int getSideB() {
-        return this.sideB;
-    }
-
-    public int getSideC() {
-        return this.sideC;
-    }
 }
--- src/test/java/TriangleTest.java
+++ src/test/java/TriangleTest.java
@@ -7,25 +7,63 @@ import static org.junit.Assert.assertEquals;
 
 public class TriangleTest {
 
-    Triangle impossible;
+    Triangle newTriangle;
 
     @Before
     public void before() {
-        impossible = new Triangle(2, 2, 5);
+        newTriangle = new Triangle();
     }
 
     @Test
-    public void canGetSideA() {
-        assertEquals(2, impossible.getSideA());
+    public void canDetermineShapeImpossibleABSmallerThanC() {
+        assertEquals("Impossible", newTriangle.determineShape(2,2,5));
+    }
+    @Test
+    public void canDetermineShapeImpossibleACSmallerThanB() {
+        assertEquals("Impossible", newTriangle.determineShape(1,2,1));
+    }
+    @Test
+    public void canDetermineShapeImpossibleBCSmallerThanA() {
+        assertEquals("Impossible", newTriangle.determineShape(15,2,1));
     }
 
     @Test
-    public void canGetSideB() {
-        assertEquals(2, impossible.getSideB());
+    public void canDetermineShapeIsoscelesAEqualBNotC() {
+        assertEquals("Isosceles", newTriangle.determineShape(4,4,5));
     }
 
     @Test
-    public void canGetSideC() {
-        assertEquals(5, impossible.getSideC());
+    public void canDetermineShapeIsoscelesAEqualCNotB() {
+        assertEquals("Isosceles", newTriangle.determineShape(2,3,2));
+    }
+
+    @Test
+    public void canDetermineShapeIsoscelesCEqualBNotA() {
+        assertEquals("Isosceles", newTriangle.determineShape(7,10,10));
+    }
+
+    @Test
+    public void canDetermineShapeIrregularASmallerThanBSmallerThanC() {
+        assertEquals("Irregular", newTriangle.determineShape(2,4,5));
+    }
+
+    @Test
+    public void canDetermineShapeIrregularABiggerThanBBiggerThanC() {
+        assertEquals("Irregular", newTriangle.determineShape(10,8,7));
+    }
+
+    @Test
+    public void canDetermineShapeIrregularBBiggerThanAABiggerThanC() {
+        assertEquals("Irregular", newTriangle.determineShape(7,12,6));
+    }
+
+    @Test
+    public void canDetermineShapeIrregularCBiggerThanAABiggerThanB() {
+        assertEquals("Irregular", newTriangle.determineShape(7,5,9));
+    }
+
+    @Test
+    public void canDetermineShapeEquilateral() {
+        assertEquals("Equilateral", newTriangle.determineShape(4,4,4));
     }
 }
