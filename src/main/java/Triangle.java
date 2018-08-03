5c305d15d4c1e8b159b14adc7a4305f8f93b41a3
1anchen <svgyc@hotmail.co.uk>
2018-08-03 01:02:15 +0100

    set up setter

---
+++ src/main/java/Triangle.java
@@ -0,0 +1,26 @@
+public class Triangle {
+
+    int sideA;
+    int sideB;
+    int sideC;
+
+    public Triangle(int numberOne, int numberTwo, int numberThree){
+
+        this.sideA=numberOne;
+        this.sideB=numberTwo;
+        this.sideC=numberThree;
+
+    }
+
+    public int getSideA() {
+        return this.sideA;
+    }
+
+    public int getSideB() {
+        return this.sideB;
+    }
+
+    public int getSideC() {
+        return this.sideC;
+    }
+}
